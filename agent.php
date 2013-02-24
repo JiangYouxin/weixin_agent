f94add876c521f529b7c49cabdf4b621b92d84fe
Jiang Youxin <jiangyouxin9@gmail.com>
2013-02-24 09:35:37 +0000

    Jiang Youxin

--- agent.php
+++ agent.php
@@ -4,6 +4,6 @@ require "_wechat.php";
 
 $agent = new WechatAgent();
 $agent->init();
-$agent->replyText("Hello World !");
+$agent->replyText("您好，这是我的微信公众账号，用来发布消息。沟通可加我的私人号 (也是QQ号): 193830。");
 
 ?>
